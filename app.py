273ddfc1582cba37b124409a3d1315c70b08c186
njitvjk <kv332@njit.edu>
2021-12-06 17:10:09 -0500

    Updated with files

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-
-from flask import Flask, redirect, url_for, request
+import flask
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+Bootstrap(app)
 
 
-@app.route('/dashboard/<name>')
-def dashboard(name):
-    return 'welcome %s' % name
+@app.route('/')
+def index():
+    # Here you could register the user.
+    # Add them to a database, for example.
+    return render_template("index.html")
 
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+@app.route('/dashboard',methods=['GET', 'POST'])
+def dashboard():
+    # flash("message test")
+    error = None
     if request.method == 'POST':
-        user = request.form['name']
-        return redirect(url_for('dashboard', name=user))
-    else:
-        user = request.args.get('name')
-        return render_template('login.html')
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            flash('Invalid credentials','danger')
+        else:
+
+            flash('You were successfully logged in','success')
+            return redirect(url_for('index'))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('dashboard.html',error=error)
+
+
+
+
+
